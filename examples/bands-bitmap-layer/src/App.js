3b2b28b7987cb9db654399b5c14ee8e219b90c24
Kyle Barron <barronk@mit.edu>
2020-04-21 01:05:37 -0600

    Update landsat url

--- examples/bands-bitmap-layer/src/App.js
+++ examples/bands-bitmap-layer/src/App.js
@@ -32,6 +32,33 @@ const initialViewState = {
   bearing: 0,
 };
 
+const MOSAIC_URL =
+  "s3://kylebarron-landsat-test/mosaics/8113f57876010a63aadacef4eac6d010d10c9aafcf36a5ece064ea7f.json.gz";
+
+function colorStr(nBands) {
+  const colorBands = "RGB".slice(0, nBands);
+  let colorStr = `gamma ${colorBands} 3.5, sigmoidal ${colorBands} 15 0.35`;
+
+  if (nBands === 3) {
+    colorStr += ", saturation 1.7";
+  }
+  return colorStr;
+}
+
+function landsatUrl(options) {
+  const { bands, url, x, y, z } = options;
+  const bandsArray = Array.isArray(bands) ? bands : [bands];
+  const params = {
+    url,
+    bands: bandsArray.join(","),
+    color_ops: colorStr(bandsArray.length),
+  };
+  const searchParams = new URLSearchParams(params);
+  let baseUrl = `https://landsat-lambda.kylebarron.dev/tiles/${z}/${x}/${y}@2x.jpg?`;
+  baseUrl += searchParams.toString();
+  return baseUrl;
+}
+
 const vibranceEffect = new PostProcessEffect(vibrance, {
   amount: 1,
 });
@@ -44,23 +71,20 @@ export default class App extends React.Component {
   render() {
     const layers = [
       new TileLayer({
-        data:
-          "https://landsat-lambda.kylebarron.dev/tiles/229bc0ed88ac7f39effdb554efa0959766e41bb3948754faba13f74f/{z}/{x}/{y}@2x.jpg?bands=&color_ops=gamma+R+3.5%2C+sigmoidal+R+15+0.35",
-
         minZoom: 0,
         maxZoom: 12,
 
-        getTileData: async ({ url, z }) => {
+        getTileData: async ({ x, y, z }) => {
           const { gl } = this.state;
           const pan = z >= 12;
 
           const urls = [
-            url.replace("bands=", "bands=4"),
-            url.replace("bands=", "bands=3"),
-            url.replace("bands=", "bands=2"),
+            landsatUrl({ x, y, z, bands: 4, url: MOSAIC_URL }),
+            landsatUrl({ x, y, z, bands: 3, url: MOSAIC_URL }),
+            landsatUrl({ x, y, z, bands: 2, url: MOSAIC_URL }),
           ];
           if (pan) {
-            urls.push(url.replace("bands=", "bands=8"));
+            urls.push(landsatUrl({ x, y, z, bands: 8, url: MOSAIC_URL }));
           }
           const images = await loadImageArray(
             urls.length,
