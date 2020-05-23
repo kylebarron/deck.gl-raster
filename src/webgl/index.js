dfff4ecf471368d82e3d2cd6c91d279a077cdb56
Kyle Barron <kylebarron2@gmail.com>
2020-05-23 14:05:26 -0600

    Move webgl modules into src/

---
+++ src/webgl/index.js
@@ -0,0 +1,9 @@
+export { default as colormap } from "./color/colormap";
+export { default as combineBands } from "./color/combine-bands";
+
+export { default as pansharpenBrovey } from "./pansharpen/pansharpen-brovey";
+
+export { default as enhancedVegetationIndex } from "./spectral-indices/evi";
+export { default as modifiedSoilAdjustedVegetationIndex } from "./spectral-indices/msavi";
+export { default as normalizedDifference } from "./spectral-indices/normalized-difference";
+export { default as soilAdjustedVegetationIndex } from "./spectral-indices/savi";
