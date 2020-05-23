b449b4963f4e0c69ad50156b47be81d419bf9dd2
Kyle Barron <kylebarron2@gmail.com>
2020-05-23 14:11:35 -0600

    Move modified SimpleMeshLayer into src/

--- src/bands-simple-mesh-layer/bands-simple-mesh-layer-fragment.js
+++ src/bands-simple-mesh-layer/bands-simple-mesh-layer-fragment.js
@@ -5,13 +5,6 @@ precision highp float;
 
 uniform bool hasTexture;
 
-uniform bool hasPan;
-uniform sampler2D bitmapTexture_r;
-uniform sampler2D bitmapTexture_g;
-uniform sampler2D bitmapTexture_b;
-uniform sampler2D bitmapTexture_pan;
-
-uniform float panWeight;
 uniform bool flatShading;
 uniform float opacity;
 
@@ -23,33 +16,12 @@ in vec4 vColor;
 
 out vec4 fragColor;
 
-// calculate pansharpen ratio
-float pansharpen_ratio(vec3 rgb, float pan, float weight) {
-    return pan / ((rgb.r + rgb.g + rgb.b * weight) / (2. + weight));
-}
-
-// Brovey Method: Each resampled, multispectral pixel is
-// multiplied by the ratio of the corresponding
-// panchromatic pixel intensity to the sum of all the
-// multispectral intensities.
-// Original code from https://github.com/mapbox/rio-pansharpen
-vec3 pansharpen(vec3 rgb, float pan, float weight) {
-    float ratio = pansharpen_ratio(rgb, pan, weight);
-    vec3 alteredRGB = ratio * rgb;
-    return clamp(alteredRGB, 0., 1.);
-}
-
 void main(void) {
   geometry.uv = vTexCoord;
+  vec4 image;
+  DECKGL_CREATE_COLOR(image, vTexCoord);
 
-  float r_band = texture2D(bitmapTexture_r, vTexCoord).r;
-  float g_band = texture2D(bitmapTexture_g, vTexCoord).r;
-  float b_band = texture2D(bitmapTexture_b, vTexCoord).r;
-  float pan_band = texture2D(bitmapTexture_pan, vTexCoord).r;
-  vec3 image = vec3(r_band, g_band, b_band);
-
-  vec3 pansharpenedImage = hasPan ? pansharpen(image, pan_band, panWeight) : image;
-  vec4 color = vec4(pansharpenedImage, 1.0);
+  DECKGL_MUTATE_COLOR(image, vTexCoord);
 
   vec3 normal;
   if (flatShading) {
@@ -67,8 +39,8 @@ void main(void) {
     normal = normals_commonspace;
   }
 
-  vec3 lightColor = lighting_getLightColor(color.rgb, cameraPosition, position_commonspace.xyz, normal);
-  fragColor = vec4(lightColor, color.a * opacity);
+  vec3 lightColor = lighting_getLightColor(image.rgb, cameraPosition, position_commonspace.xyz, normal);
+  fragColor = vec4(lightColor, opacity);
 
   DECKGL_FILTER_COLOR(fragColor, geometry);
 }
