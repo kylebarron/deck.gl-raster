376b9fcd33df9d35b0c49c4898af2f651fd1f0f5
Kyle Barron <kylebarron2@gmail.com>
2020-05-23 13:55:06 -0600

    Move updated code back to src folder

--- src/bands-bitmap-layer/bands-bitmap-layer-fragment.js
+++ src/bands-bitmap-layer/bands-bitmap-layer-fragment.js
@@ -5,42 +5,12 @@ export default `
 precision highp float;
 #endif
 
-uniform sampler2D bitmapTexture_r;
-uniform sampler2D bitmapTexture_g;
-uniform sampler2D bitmapTexture_b;
-uniform sampler2D bitmapTexture_pan;
-uniform sampler2D bitmapTexture_colormap;
-
 varying vec2 vTexCoord;
-
-uniform bool usePan;
-uniform bool useRgb;
-uniform bool useNdvi;
-uniform bool useEvi;
-uniform bool useSavi;
-uniform bool useMsavi;
-uniform float panWeight;
 
 uniform float desaturate;
 uniform vec4 transparentColor;
 uniform vec3 tintColor;
 uniform float opacity;
-
-// calculate pansharpen ratio
-float pansharpen_ratio(vec3 rgb, float pan, float weight) {
-    return pan / ((rgb.r + rgb.g + rgb.b * weight) / (2. + weight));
-}
-
-// Brovey Method: Each resampled, multispectral pixel is
-// multiplied by the ratio of the corresponding
-// panchromatic pixel intensity to the sum of all the
-// multispectral intensities.
-// Original code from https://github.com/mapbox/rio-pansharpen
-vec3 pansharpen(vec3 rgb, float pan, float weight) {
-    float ratio = pansharpen_ratio(rgb, pan, weight);
-    vec3 alteredRGB = ratio * rgb;
-    return clamp(alteredRGB, 0., 1.);
-}
 
 // apply desaturation
 vec3 color_desaturate(vec3 color) {
@@ -58,71 +28,11 @@ vec4 apply_opacity(vec3 color, float alpha) {
   return mix(transparentColor, vec4(color, 1.0), alpha);
 }
 
-// Calculate standard normalized difference
-float normalized_difference(float band1, float band2) {
-  return ((band1 - band2) / (band1 + band2));
-}
-
-// Calculate enhanced vegetation index
-// EVI = 2.5 * ((Band 5 – Band 4) / (Band 5 + 6 * Band 4 – 7.5 * Band 2 + 1))
-// https://www.usgs.gov/land-resources/nli/landsat/landsat-enhanced-vegetation-index
-float enhanced_vegetation_index(float band5, float band4, float band2) {
-  float numerator = band5 - band4;
-  float denominator = band5 + (6. * band4) - (7.5 * band2) + 1.;
-  return 2.5 * (numerator / denominator);
-}
-
-// Calculate soil-adjusted vegetation index
-// SAVI = ((Band 5 – Band 4) / (Band 5 + Band 4 + 0.5)) * (1.5).
-// https://www.usgs.gov/land-resources/nli/landsat/landsat-soil-adjusted-vegetation-index
-float soil_adjusted_vegetation_index(float band5, float band4) {
-  float numerator = band5 - band4;
-  float denominator = (band5 + band4 + 0.5) * 1.5;
-  return numerator / denominator;
-}
-
-// Calculate modified soil-adjusted vegetation index
-// MSAVI = (2 * Band 5 + 1 – sqrt ((2 * Band 5 + 1)^2 – 8 * (Band 5 – Band 4))) / 2
-// https://www.usgs.gov/land-resources/nli/landsat/landsat-modified-soil-adjusted-vegetation-index
-float modified_soil_adjusted_vegetation_index(float band5, float band4) {
-  float to_sqrt = ((2. * band5 + 1.) * (2. * band5 + 1.)) - (8. * (band5 - band4));
-  return ((2. * band5) + 1. - sqrt(to_sqrt)) / 2.;
-}
-
-// Apply colormap texture given value
-// Since the texture only varies in the x direction, setting v to 0.5 as a
-// constant is fine
-// Assumes the input range of value is -1 to 1
-vec3 apply_colormap(sampler2D colormap, float value) {
-  vec2 uv = vec2(0.5 * value + 0.5, 0.5);
-  return texture2D(colormap, uv).rgb;
-}
-
 void main(void) {
-  float r_band = texture2D(bitmapTexture_r, vTexCoord).r;
-  float g_band = texture2D(bitmapTexture_g, vTexCoord).r;
-  float b_band = texture2D(bitmapTexture_b, vTexCoord).r;
-  float pan_band = texture2D(bitmapTexture_pan, vTexCoord).r;
+  vec4 image;
+  DECKGL_CREATE_COLOR(image, vTexCoord);
 
-  vec3 image;
-  if (useRgb) {
-    image = vec3(r_band, g_band, b_band);
-    if (usePan) {
-      image = pansharpen(image, pan_band, panWeight);
-    }
-  } else {
-    float value;
-    if (useNdvi) {
-      value = normalized_difference(r_band, g_band);
-    } else if (useEvi) {
-      value = enhanced_vegetation_index(r_band, g_band, b_band);
-    } else if (useSavi) {
-      value = soil_adjusted_vegetation_index(r_band, g_band);
-    } else if (useMsavi) {
-      value = modified_soil_adjusted_vegetation_index(r_band, g_band);
-    }
-    image = apply_colormap(bitmapTexture_colormap, value);
-  }
+  DECKGL_MUTATE_COLOR(image, vTexCoord);
 
   gl_FragColor = apply_opacity(color_tint(color_desaturate(image.rgb)), opacity);
 
--- src/bands-bitmap-layer/bands-bitmap-layer.js
+++ src/bands-bitmap-layer/bands-bitmap-layer.js
@@ -1,193 +1,78 @@
 import GL from "@luma.gl/constants";
 import { BitmapLayer } from "@deck.gl/layers";
-import { Texture2D } from "@luma.gl/core";
+import { Model, Geometry } from "@luma.gl/core";
+import { project32, picking } from "@deck.gl/core";
 
 import fs from "./bands-bitmap-layer-fragment";
-
-const DEFAULT_TEXTURE_PARAMETERS = {
-  [GL.TEXTURE_MIN_FILTER]: GL.LINEAR_MIPMAP_LINEAR,
-  [GL.TEXTURE_MAG_FILTER]: GL.LINEAR,
-  [GL.TEXTURE_WRAP_S]: GL.CLAMP_TO_EDGE,
-  [GL.TEXTURE_WRAP_T]: GL.CLAMP_TO_EDGE,
-};
+import { ProgramManager } from "@luma.gl/engine";
 
 const defaultProps = {
-  image_r: { type: "object", value: null, async: true },
-  image_g: { type: "object", value: null, async: true },
-  image_b: { type: "object", value: null, async: true },
-  image_pan: { type: "object", value: null, async: true },
-  colormap: { type: "object", value: null, async: true },
-
-  // Method of combining bands, one of:
-  // "rgb", "normalized_difference", 'evi', 'savi', 'msavi'
-  band_combination: "rgb",
-
-  bounds: { type: "array", value: [1, 0, 0, 1], compare: true },
-  desaturate: { type: "number", min: 0, max: 1, value: 0 },
-  // Weight of blue band
-  panWeight: { type: "number", min: 0, max: 1, value: 0.2 },
-  // Optionally turn off pansharpening
-  usePan: { type: "boolean", value: true },
-  // More context: because of the blending mode we're using for ground imagery,
-  // alpha is not effective when blending the bitmap layers with the base map.
-  // Instead we need to manually dim/blend rgb values with a background color.
-  transparentColor: { type: "color", value: [0, 0, 0, 0] },
-  tintColor: { type: "color", value: [255, 255, 255] },
+  ...BitmapLayer.defaultProps,
+  modules: { type: "array", value: [], compare: true },
+  asyncModuleUniforms: {
+    type: "object",
+    value: {},
+    compare: true,
+    async: true,
+  },
+  moduleUniforms: { type: "object", value: {}, compare: true },
 };
 
 export default class BandsBitmapLayer extends BitmapLayer {
-  draw(opts) {
-    const { uniforms } = opts;
-    const {
-      bitmapTexture_r,
-      bitmapTexture_g,
-      bitmapTexture_b,
-      bitmapTexture_pan,
-      bitmapTexture_colormap,
-      model,
-    } = this.state;
+  draw({ uniforms }) {
+    const { model } = this.state;
     const {
       desaturate,
       transparentColor,
       tintColor,
-      panWeight,
-      band_combination,
+      moduleUniforms,
+      asyncModuleUniforms,
     } = this.props;
 
-    let usePan = false;
-    let useNdvi = false;
-    let useRgb = false;
-    let useEvi = false;
-    let useSavi = false;
-    let useMsavi = false;
-
-    switch (band_combination.toLowerCase()) {
-      case "rgb":
-        useRgb = true;
-        usePan = Boolean(bitmapTexture_pan) && this.props.usePan;
-        break;
-      case "normalized_difference":
-        useNdvi = true;
-        break;
-      case "evi":
-        useEvi = true;
-        break;
-      case "savi":
-        useSavi = true;
-        break;
-      case "msavi":
-        useMsavi = true;
-        break;
-      default:
-        console.error(`Invalid band_combination: ${band_combination}`);
-    }
-
-    // // TODO fix zFighting
     // Render the image
-    if (bitmapTexture_r && bitmapTexture_g && model) {
-      model
-        .setUniforms(
-          Object.assign({}, uniforms, {
-            // Textures
-            bitmapTexture_r,
-            bitmapTexture_g,
-            bitmapTexture_b,
-            bitmapTexture_colormap,
+    //
+    // Wait for asyncModuleUniforms to have >=1 truthy key before rendering
+    // Important to prevent both flickering and "instanced rendering of wrong
+    // data". Without this check, sometimes when panning to a new area, new
+    // tiles will be a checkerboard of one existing tile while waiting for the
+    // new textures to load.
+    if (
+      !model ||
+      Object.keys(asyncModuleUniforms).length === 0 ||
+      !Object.values(asyncModuleUniforms).every((item) => item)
+    ) {
+      return;
+    }
 
-            // General image options
-            desaturate,
-            transparentColor: transparentColor.map((x) => x / 255),
-            tintColor: tintColor.slice(0, 3).map((x) => x / 255),
-
-            // Pan options, pan texture may or may not exist
-            bitmapTexture_pan,
-            panWeight,
-
-            // Image operations
-            usePan,
-            useNdvi,
-            useRgb,
-            useEvi,
-            useSavi,
-            useMsavi,
-          })
-        )
-        .draw();
-    }
-  }
-
-  finalizeState() {
-    super.finalizeState();
-
-    if (this.state.bitmapTexture_r) {
-      this.state.bitmapTexture_r.delete();
-    }
-    if (this.state.bitmapTexture_g) {
-      this.state.bitmapTexture_g.delete();
-    }
-    if (this.state.bitmapTexture_b) {
-      this.state.bitmapTexture_b.delete();
-    }
-    if (this.state.bitmapTexture_pan) {
-      this.state.bitmapTexture_pan.delete();
-    }
-    if (this.state.bitmapTexture_colormap) {
-      this.state.bitmapTexture_colormap.delete();
-    }
+    model
+      .setUniforms(
+        Object.assign({}, uniforms, {
+          desaturate,
+          transparentColor: transparentColor.map((x) => x / 255),
+          tintColor: tintColor.slice(0, 3).map((x) => x / 255),
+        })
+      )
+      .updateModuleSettings({ ...asyncModuleUniforms, ...moduleUniforms })
+      .draw();
   }
 
   getShaders() {
-    // use object.assign to make sure we don't overwrite existing fields like `vs`, `modules`...
+    const { modules } = this.props;
     return Object.assign({}, super.getShaders(), {
       fs,
+      modules: [project32, picking, ...modules],
     });
   }
 
   updateState({ props, oldProps, changeFlags }) {
     // setup model first
-    if (changeFlags.extensionsChanged) {
+    if (changeFlags.extensionsChanged || props.modules !== oldProps.modules) {
       const { gl } = this.context;
       if (this.state.model) {
         this.state.model.delete();
       }
       this.setState({ model: this._getModel(gl) });
       this.getAttributeManager().invalidateAll();
-    }
-
-    if (props.image_r !== oldProps.image_r) {
-      const bitmapTexture_r = this.loadTexture(props.image_r);
-      if (this.state.bitmapTexture_r) {
-        this.state.bitmapTexture_r.delete();
-      }
-      this.setState({ bitmapTexture_r });
-    }
-    if (props.image_g !== oldProps.image_g) {
-      const bitmapTexture_g = this.loadTexture(props.image_g);
-      if (this.state.bitmapTexture_g) {
-        this.state.bitmapTexture_g.delete();
-      }
-      this.setState({ bitmapTexture_g });
-    }
-    if (props.image_b !== oldProps.image_b) {
-      const bitmapTexture_b = this.loadTexture(props.image_b);
-      if (this.state.bitmapTexture_b) {
-        this.state.bitmapTexture_b.delete();
-      }
-      this.setState({ bitmapTexture_b });
-    }
-    if (props.image_pan !== oldProps.image_pan) {
-      const bitmapTexture_pan = this.loadTexture(props.image_pan);
-      if (this.state.bitmapTexture_pan) {
-        this.state.bitmapTexture_pan.delete();
-      }
-      this.setState({ bitmapTexture_pan });
-    }
-    if (props.colormap !== oldProps.colormap) {
-      const bitmapTexture_colormap = this.loadTexture(props.colormap);
-      if (this.state.bitmapTexture_colormap) {
-        this.state.bitmapTexture_colormap.delete();
-      }
-      this.setState({ bitmapTexture_colormap });
     }
 
     const attributeManager = this.getAttributeManager();
@@ -197,30 +82,37 @@ export default class BandsBitmapLayer extends BitmapLayer {
     }
   }
 
-  loadTexture(image) {
-    const { gl } = this.context;
-
-    if (image instanceof Texture2D) {
-      return image;
-    } else if (image instanceof HTMLVideoElement) {
-      // Initialize an empty texture while we wait for the video to load
-      return {
-        bitmapTexture: new Texture2D(gl, {
-          width: 1,
-          height: 1,
-          parameters: DEFAULT_TEXTURE_PARAMETERS,
-          mipmaps: false,
-        }),
-      };
-    } else if (image) {
-      // Browser object: Image, ImageData, HTMLCanvasElement, ImageBitmap
-      return {
-        bitmapTexture: new Texture2D(gl, {
-          data: image,
-          parameters: DEFAULT_TEXTURE_PARAMETERS,
-        }),
-      };
+  _getModel(gl) {
+    if (!gl) {
+      return null;
     }
+    ProgramManager.getDefaultProgramManager(gl).addShaderHook(
+      "fs:DECKGL_MUTATE_COLOR(inout vec4 image, in vec2 coord)"
+    );
+
+    ProgramManager.getDefaultProgramManager(gl).addShaderHook(
+      "fs:DECKGL_CREATE_COLOR(inout vec4 image, in vec2 coord)"
+    );
+
+    /*
+      0,0 --- 1,0
+       |       |
+      0,1 --- 1,1
+    */
+    return new Model(
+      gl,
+      Object.assign({}, this.getShaders(), {
+        id: this.props.id,
+        geometry: new Geometry({
+          drawMode: GL.TRIANGLE_FAN,
+          vertexCount: 4,
+          attributes: {
+            texCoords: new Float32Array([0, 1, 0, 0, 1, 0, 1, 1]),
+          },
+        }),
+        isInstanced: false,
+      })
+    );
   }
 }
 
