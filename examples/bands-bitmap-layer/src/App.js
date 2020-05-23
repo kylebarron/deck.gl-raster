be39189480d7c86f4791ec7cd5b91934bee21db8
Kyle Barron <kylebarron2@gmail.com>
2020-05-23 14:14:58 -0600

    Move promiseAllObject into src/util

--- examples/bands-bitmap-layer/src/App.js
+++ examples/bands-bitmap-layer/src/App.js
@@ -138,18 +138,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-// https://lowrey.me/lodash-zipobject-in-es6-javascript/
-const zipObject = (props, values) => {
-  return props.reduce((prev, prop, i) => {
-    return Object.assign(prev, { [prop]: values[i] });
-  }, {});
-};
-
-// https://stackoverflow.com/a/50437423
-const promiseAllObj = async (object) => {
-  return zipObject(
-    Object.keys(object),
-    await Promise.all(Object.values(object))
-  );
-};
