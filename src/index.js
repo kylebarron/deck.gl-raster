c2eccf9674089fc0a4f0ee7b6765d0ba2b1279b6
Kyle Barron <kylebarron2@gmail.com>
2020-05-24 00:03:07 -0600

    Fix index paths

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-export { default as RasterLayer } from "./raster-layer/raster-layer";
-export { default as RasterMeshLayer } from "./raster-mesh-layer/raster-mesh-layer";
+export { default as RasterLayer } from "./deckgl/raster-layer/raster-layer";
+export { default as RasterMeshLayer } from "./deckgl/raster-mesh-layer/raster-mesh-layer";
 
-export * from "./util";
-export * from "./webgl";
+export * from "./util/index";
+export * from "./webgl/index";
