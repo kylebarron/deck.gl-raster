ab608c30a152b65362a92904f2a6d631c5dc3c0a
Kyle Barron <kylebarron2@gmail.com>
2020-07-11 18:30:21 -0600

    Remove React Map GL in Mesh Example for performance

--- examples/raster-mesh-layer/src/App.js
+++ examples/raster-mesh-layer/src/App.js
@@ -1,7 +1,6 @@
 /* eslint-disable max-statements */
 import React from 'react';
 import DeckGL from '@deck.gl/react';
-import {StaticMap} from 'react-map-gl';
 import {TerrainTileLayer} from './terrain-tile-layer';
 
 const INITIAL_VIEW_STATE = {
@@ -31,12 +30,7 @@ export default class App extends React.Component {
         initialViewState={INITIAL_VIEW_STATE}
         controller={true}
         layers={layers}
-      >
-        <StaticMap
-          mapStyle="https://raw.githubusercontent.com/kylebarron/fiord-color-gl-style/master/style.json"
-          mapOptions={{ hash: true }}
-        />
-      </DeckGL>
+      />
     );
   }
 }
