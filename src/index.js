b068074313eabda57998cd04b68ae9977352ab93
Kyle Barron <kylebarron2@gmail.com>
2020-08-07 22:04:49 -0600

    Remove extra unused util functions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 export { default as RasterLayer } from "./deckgl/raster-layer/raster-layer";
 export { default as RasterMeshLayer } from "./deckgl/raster-mesh-layer/raster-mesh-layer";
 
-export * from "./util/index";
 export * from "./webgl/index";
