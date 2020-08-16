cf3fe79ab614b8b3c6487265de5a63a96f49de8b
Kyle Barron <kylebarron2@gmail.com>
2020-08-16 15:53:39 -0700

    Deployed 3afb839 with MkDocs version: 1.1.2

---
+++ examples/raster-layer/precache-manifest.329c7e4a063dc9272f4319a2c05a64c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d79f18ac0f86f618b4309e91909ce6fd",
+    "url": "/deck.gl-raster/examples/raster-layer/index.html"
+  },
+  {
+    "revision": "8f38eb8c1c25d0496b0f",
+    "url": "/deck.gl-raster/examples/raster-layer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0943b5b6ad714abb3caa",
+    "url": "/deck.gl-raster/examples/raster-layer/static/js/2.14b8d959.chunk.js"
+  },
+  {
+    "revision": "0fb0e672f1308cb5b7677cdb93fc42ab",
+    "url": "/deck.gl-raster/examples/raster-layer/static/js/2.14b8d959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f38eb8c1c25d0496b0f",
+    "url": "/deck.gl-raster/examples/raster-layer/static/js/main.a6fa1bd3.chunk.js"
+  },
+  {
+    "revision": "842cf3b6e99d3b3758b9",
+    "url": "/deck.gl-raster/examples/raster-layer/static/js/runtime-main.6db92224.js"
+  }
+]);
--- examples/raster-layer/service-worker.js
+++ examples/raster-layer/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/examples/raster-layer/precache-manifest.5c4ef51fda87c90285a25fbd46ecac6e.js"
+  "/deck.gl-raster/examples/raster-layer/precache-manifest.329c7e4a063dc9272f4319a2c05a64c8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/examples/raster-layer/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/deck.gl-raster/examples/raster-layer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
