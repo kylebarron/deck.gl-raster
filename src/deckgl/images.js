26a21b59524dc25bd8f319b0d8e0c72c21501933
Kyle Barron <kylebarron2@gmail.com>
2020-09-16 21:38:51 -0600

    Add fallbacks for webgl1

--- src/deckgl/images.js
+++ src/deckgl/images.js
@@ -1,4 +1,4 @@
-import {Texture2D} from '@luma.gl/core';
+import {isWebGL2, Texture2D} from '@luma.gl/core';
 import GL from '@luma.gl/constants';
 import isEqual from 'lodash.isequal';
 
@@ -10,6 +10,8 @@ const DEFAULT_TEXTURE_PARAMETERS = {
 };
 
 export function loadImages({gl, images, props, oldProps}) {
+  const webgl2 = isWebGL2(gl);
+
   // Change to `true` if we need to setState with a new `images` object
   let imagesDirty = false;
 
@@ -47,9 +49,9 @@ export function loadImages({gl, images, props, oldProps}) {
     }
 
     if (Array.isArray(imageData)) {
-      images[key] = imageData.map((x) => loadTexture(gl, x));
+      images[key] = imageData.map((x) => loadTexture(gl, x, webgl2));
     } else {
-      images[key] = loadTexture(gl, imageData);
+      images[key] = loadTexture(gl, imageData, webgl2);
     }
     imagesDirty = true;
   }
@@ -61,13 +63,53 @@ export function loadImages({gl, images, props, oldProps}) {
   return null;
 }
 
-function loadTexture(gl, imageData) {
+function loadTexture(gl, imageData, webgl2) {
   if (imageData instanceof Texture2D) {
     return imageData;
   } else if (imageData) {
-    return new Texture2D(gl, {
+    const textureParams = {
       parameters: DEFAULT_TEXTURE_PARAMETERS,
       ...imageData,
-    });
+    };
+
+    // WIP fallbacks to WebGL1
+    // Fallback ideas derived from viv
+    // https://github.com/hms-dbmi/viv/blob/5bcec429eeba55914ef3d7155a610d82048520a0/src/layers/XRLayer/XRLayer.js#L280-L302
+    if (!webgl2) {
+      // Set mipmaps to false
+      // Not sure if this is necessary?
+      // Might actually only be necessary for uint textures
+      textureParams.mipmaps = false;
+
+      // Change format to Luminance
+      if ([GL.R8UI, GL.R16UI, GL.R32UI].includes(textureParams.format)) {
+        textureParams.format = GL.LUMINANCE;
+      }
+
+      // Change dataFormat to Luminance
+      if (textureParams.dataFormat === GL.RED_INTEGER) {
+        textureParams.dataFormat = GL.LUMINANCE;
+      }
+
+      // Set data type to float
+      if (
+        [GL.UNSIGNED_BYTE, GL.UNSIGNED_SHORT, GL.UNSIGNED_INT].includes(
+          textureParams.type
+        )
+      ) {
+        textureParams.type = GL.FLOAT;
+      }
+
+      // Cast data to float 32 if one of the uint types
+      if (
+        textureParams.data instanceof Uint8Array ||
+        textureParams.data instanceof Uint16Array ||
+        textureParams.data instanceof Uint32Array
+      ) {
+        textureParams.data = new Float32Array(textureParams.data);
+      }
+    }
+
+    return new Texture2D(gl, textureParams);
   }
 }
