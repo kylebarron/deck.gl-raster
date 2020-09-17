8b96fd7612af2ae857fa17ab17293774755eae65
Kyle Barron <kylebarron2@gmail.com>
2020-09-16 22:50:42 -0600

    Separate gamma values per band

--- src/webgl/color/gamma-contrast.fs.glsl
+++ src/webgl/color/gamma-contrast.fs.glsl
@@ -1,6 +1,9 @@
 const float epsilon = 0.00000001;
 
-uniform float gamma_value;
+uniform float gamma_r;
+uniform float gamma_g;
+uniform float gamma_b;
+uniform float gamma_a;
 
 // Gamma correction is a nonlinear operation that
 // adjusts the image's channel values pixel-by-pixel according
@@ -18,9 +21,18 @@ uniform float gamma_value;
 
 // NOTE: Input array must have float values between 0 and 1!
 // NOTE: gamma must be >= 0
-vec4 gammaContrast(vec4 arr, float g) {
-  // Gamma must be >= 0
+float gammaContrast(float arr, float g) {
+  // Gamma must be > 0
   g = g - epsilon <= 0. ? epsilon : g;
 
-  return pow(arr, vec4(1.0 / g));
+  return pow(arr, 1.0 / g);
+}
+
+vec4 gammaContrast(vec4 arr, float gamma1, float gamma2, float gamma3, float gamma4) {
+  arr.r = gammaContrast(arr.r, gamma1);
+  arr.g = gammaContrast(arr.g, gamma2);
+  arr.b = gammaContrast(arr.b, gamma3);
+  arr.a = gammaContrast(arr.a, gamma4);
+
+  return arr;
 }
--- src/webgl/color/gamma-contrast.js
+++ src/webgl/color/gamma-contrast.js
@@ -1,10 +1,13 @@
 import fs from './gamma-contrast.fs.glsl';
 
 function getUniforms(opts = {}) {
-  const {gammaValue = 1} = opts;
+  const {gammaR = 1, gammaG = 1, gammaB = 1, gammaA = 1} = opts;
 
   return {
-    gamma_value: gammaValue,
+    gamma_r: gammaR,
+    gamma_g: gammaG,
+    gamma_b: gammaB,
+    gamma_a: gammaA,
   };
 }
 
@@ -14,7 +17,7 @@ export default {
   getUniforms,
   inject: {
     'fs:DECKGL_MUTATE_COLOR': `
-    image = gammaContrast(image, gamma_value);
+    image = gammaContrast(image, gamma_r, gamma_g, gamma_b, gamma_a);
     `,
   },
 };
