60adfab7a22b4fb77dff466b82935bbd1e31d4ca
Kyle Barron <kylebarron2@gmail.com>
2020-09-20 10:14:07 -0600

    Refactor image fallbacks on webgl1

--- src/deckgl/images.js
+++ src/deckgl/images.js
@@ -10,8 +10,6 @@ const DEFAULT_TEXTURE_PARAMETERS = {
 };
 
 export function loadImages({gl, images, props, oldProps}) {
-  const webgl2 = isWebGL2(gl);
-
   // Change to `true` if we need to setState with a new `images` object
   let imagesDirty = false;
 
@@ -49,9 +47,9 @@ export function loadImages({gl, images, props, oldProps}) {
     }
 
     if (Array.isArray(imageData)) {
-      images[key] = imageData.map((x) => loadTexture(gl, x, webgl2));
+      images[key] = imageData.map((x) => loadTexture(gl, x));
     } else {
-      images[key] = loadTexture(gl, imageData, webgl2);
+      images[key] = loadTexture(gl, imageData);
     }
     imagesDirty = true;
   }
@@ -63,53 +61,77 @@ export function loadImages({gl, images, props, oldProps}) {
   return null;
 }
 
-function loadTexture(gl, imageData, webgl2) {
+/**
+ * Create Texture2D object from image data
+ *
+ * @param   {any}  gl gl context
+ * @param   {Texture2D | Object} imageData input object representing image
+ *
+ * @return  {Texture2D?} Texture2D object representing image
+ */
+function loadTexture(gl, imageData) {
+  if (!imageData) {
+    return null;
+  }
+
   if (imageData instanceof Texture2D) {
     return imageData;
-  } else if (imageData) {
-    const textureParams = {
-      parameters: DEFAULT_TEXTURE_PARAMETERS,
-      ...imageData,
-    };
-
-    // WIP fallbacks to WebGL1
-    // Fallback ideas derived from viv
-    // https://github.com/hms-dbmi/viv/blob/5bcec429eeba55914ef3d7155a610d82048520a0/src/layers/XRLayer/XRLayer.js#L280-L302
-    if (!webgl2) {
-      // Set mipmaps to false
-      // Not sure if this is necessary?
-      // Might actually only be necessary for uint textures
-      textureParams.mipmaps = false;
-
-      // Change format to Luminance
-      if ([GL.R8UI, GL.R16UI, GL.R32UI].includes(textureParams.format)) {
-        textureParams.format = GL.LUMINANCE;
-      }
-
-      // Change dataFormat to Luminance
-      if (textureParams.dataFormat === GL.RED_INTEGER) {
-        textureParams.dataFormat = GL.LUMINANCE;
-      }
-
-      // Set data type to float
-      if (
-        [GL.UNSIGNED_BYTE, GL.UNSIGNED_SHORT, GL.UNSIGNED_INT].includes(
-          textureParams.type
-        )
-      ) {
-        textureParams.type = GL.FLOAT;
-      }
-
-      // Cast data to float 32 if one of the uint types
-      if (
-        textureParams.data instanceof Uint8Array ||
-        textureParams.data instanceof Uint16Array ||
-        textureParams.data instanceof Uint32Array
-      ) {
-        textureParams.data = new Float32Array(textureParams.data);
-      }
-    }
-
-    return new Texture2D(gl, textureParams);
   }
+
+  let textureParams = {
+    parameters: DEFAULT_TEXTURE_PARAMETERS,
+    ...imageData,
+  };
+
+  if (!isWebGL2(gl)) {
+    textureParams = webgl1TextureFallbacks(textureParams);
+  }
+
+  return new Texture2D(gl, textureParams);
+}
+
+/**
+ * Texture fallbacks for WebGL1
+ * Fallback ideas derived from viv
+ * https://github.com/hms-dbmi/viv/blob/5bcec429eeba55914ef3d7155a610d82048520a0/src/layers/XRLayer/XRLayer.js#L280-L302
+ *
+ * @param   {Object}  textureParams  [textureParams description]
+ *
+ * @return  {Object}                 [return description]
+ */
+function webgl1TextureFallbacks(textureParams) {
+  // Set mipmaps to false
+  // Not sure if this is necessary?
+  // Might actually only be necessary for uint textures
+  textureParams.mipmaps = false;
+
+  // Change format to Luminance
+  if ([GL.R8UI, GL.R16UI, GL.R32UI].includes(textureParams.format)) {
+    textureParams.format = GL.LUMINANCE;
+  }
+
+  // Change dataFormat to Luminance
+  if (textureParams.dataFormat === GL.RED_INTEGER) {
+    textureParams.dataFormat = GL.LUMINANCE;
+  }
+
+  // Set data type to float
+  if (
+    [GL.UNSIGNED_BYTE, GL.UNSIGNED_SHORT, GL.UNSIGNED_INT].includes(
+      textureParams.type
+    )
+  ) {
+    textureParams.type = GL.FLOAT;
+  }
+
+  // Cast data to float 32 if one of the uint types
+  if (
+    textureParams.data instanceof Uint8Array ||
+    textureParams.data instanceof Uint16Array ||
+    textureParams.data instanceof Uint32Array
+  ) {
+    textureParams.data = new Float32Array(textureParams.data);
+  }
+
+  return textureParams;
 }
