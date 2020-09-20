24b83b9eeb08174a1cb85765e6b251786c2b55a1
Kyle Barron <kylebarron2@gmail.com>
2020-09-20 10:24:38 -0600

    Move numpy loader into src/

--- src/loaders/npy-loader.js
+++ src/loaders/npy-loader.js
@@ -1,3 +1,8 @@
+// NOTE: this should be considered an experimental export for now
+
+// \x93NUMPY
+const NPY_MAGIC = new Uint8Array([147, 78, 85, 77, 80, 89]);
+
 // The basic string format consists of 3 parts:
 // - a character describing the byteorder of the data (<: little-endian, >: big-endian, |: not-relevant)
 // - a character code giving the basic type of the array
@@ -6,65 +11,51 @@
 const DTYPES = {
   '<u1': {
     name: 'uint8',
-    size: 8,
     arrayConstructor: Uint8Array,
   },
   '|u1': {
     name: 'uint8',
-    size: 8,
     arrayConstructor: Uint8Array,
   },
   '|i1': {
     name: 'int8',
-    size: 8,
     arrayConstructor: Int8Array,
   },
   '<u2': {
     name: 'uint16',
-    size: 16,
     arrayConstructor: Uint16Array,
   },
   '<i2': {
     name: 'int16',
-    size: 16,
     arrayConstructor: Int16Array,
   },
   '<u4': {
     name: 'uint32',
-    size: 32,
     arrayConstructor: Int32Array,
   },
   '<i4': {
     name: 'int32',
-    size: 32,
     arrayConstructor: Int32Array,
   },
   // '<u8': {
   //   name: 'uint64',
-  //   size: 64,
   //   arrayConstructor: BigUint64Array,
   // },
   // '<i8': {
   //   name: 'int64',
-  //   size: 64,
   //   arrayConstructor: BigInt64Array,
   // },
   '<f4': {
     name: 'float32',
-    size: 32,
     arrayConstructor: Float32Array,
   },
   '<f8': {
     name: 'float64',
-    size: 64,
     arrayConstructor: Float64Array,
   },
 };
 
-// \x93NUMPY
-const NPY_MAGIC = new Uint8Array([147, 78, 85, 77, 80, 89]);
-
-export function parse(arrayBuffer) {
+export function parseNpy(arrayBuffer) {
   const view = new DataView(arrayBuffer);
 
   const magic = new Uint8Array(arrayBuffer, 0, 6);
