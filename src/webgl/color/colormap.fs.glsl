3dd190d993bb2d4046adc29fdfbc05b98371576f
Kyle Barron <kylebarron2@gmail.com>
2020-09-21 22:32:51 -0600

    Rename colormap glsl function

--- src/webgl/color/colormap.fs.glsl
+++ src/webgl/color/colormap.fs.glsl
@@ -1,10 +1,10 @@
-uniform sampler2D bitmapTexture_colormap;
+uniform sampler2D u_colormap_texture;
 
 // Apply colormap texture given value
 // Since the texture only varies in the x direction, setting v to 0.5 as a
 // constant is fine
 // Assumes the input range of value is -1 to 1
-vec4 colormap_apply(sampler2D colormap, vec4 image) {
+vec4 colormap(sampler2D cmap, vec4 image) {
   vec2 uv = vec2(0.5 * image.r + 0.5, 0.5);
-  return texture2D(colormap, uv);
+  return texture2D(cmap, uv);
 }
