9e6f7faf94fdd5c86c89d3e4ccae113335355ecc
Jan Zak <zj@zakjan.cz>
2021-04-30 07:06:46 +0200

    Add support for BitmapLayer._imageCoordinateSystem

--- src/deckgl/raster-layer/raster-layer-webgl2.fs.glsl
+++ src/deckgl/raster-layer/raster-layer-webgl2.fs.glsl
@@ -7,6 +7,7 @@ precision mediump int;
 precision mediump usampler2D;
 
 in vec2 vTexCoord;
+in vec2 vTexPos;
 
 out vec4 color;
 
@@ -14,6 +15,34 @@ uniform float desaturate;
 uniform vec4 transparentColor;
 uniform vec3 tintColor;
 uniform float opacity;
+
+uniform mediump float coordinateConversion;
+uniform vec4 bounds;
+
+/* projection utils */
+const float TILE_SIZE = 512.0;
+const float PI = 3.1415926536;
+const float WORLD_SCALE = TILE_SIZE / PI / 2.0;
+
+// from degrees to Web Mercator
+vec2 lnglat_to_mercator(vec2 lnglat) {
+  float x = lnglat.x;
+  float y = clamp(lnglat.y, -89.9, 89.9);
+  return vec2(
+    radians(x) + PI,
+    PI + log(tan(PI * 0.25 + radians(y) * 0.5))
+  ) * WORLD_SCALE;
+}
+
+// from Web Mercator to degrees
+vec2 mercator_to_lnglat(vec2 xy) {
+  xy /= WORLD_SCALE;
+  return degrees(vec2(
+    xy.x - PI,
+    atan(exp(xy.y - PI)) * 2.0 - PI * 0.5
+  ));
+}
+/* End projection utils */
 
 // apply desaturation
 vec3 color_desaturate(vec3 color) {
@@ -31,14 +60,29 @@ vec4 apply_opacity(vec3 color, float alpha) {
   return mix(transparentColor, vec4(color, 1.0), alpha);
 }
 
-void main(void) {
-  vec4 image;
-  DECKGL_CREATE_COLOR(image, vTexCoord);
+vec2 getUV(vec2 pos) {
+  return vec2(
+    (pos.x - bounds[0]) / (bounds[2] - bounds[0]),
+    (pos.y - bounds[3]) / (bounds[1] - bounds[3])
+  );
+}
 
-  DECKGL_MUTATE_COLOR(image, vTexCoord);
+void main(void) {
+  vec2 uv = vTexCoord;
+  if (coordinateConversion < -0.5) {
+    vec2 lnglat = mercator_to_lnglat(vTexPos);
+    uv = getUV(lnglat);
+  } else if (coordinateConversion > 0.5) {
+    vec2 commonPos = lnglat_to_mercator(vTexPos);
+    uv = getUV(commonPos);
+  }
+  vec4 image;
+  DECKGL_CREATE_COLOR(image, uv);
+
+  DECKGL_MUTATE_COLOR(image, uv);
 
   color = apply_opacity(color_tint(color_desaturate(image.rgb)), opacity);
 
-  geometry.uv = vTexCoord;
+  geometry.uv = uv;
   DECKGL_FILTER_COLOR(color, geometry);
 }
